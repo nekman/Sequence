dd965b0668b9ae59a9fd4225699dea21e9832037
Nils Ekman <nisse.ekman@gmail.com>
2012-12-02 16:30:34 +0100

    added javadoc

--- src/main/java/se/nekman/sequence/selectors/Action.java
+++ src/main/java/se/nekman/sequence/selectors/Action.java
@@ -1,5 +1,15 @@
 package se.nekman.sequence.selectors;
 
+/**
+ * 
+ * @author nekman
+ *
+ * @param <T>
+ */
 public interface Action<T> {
+	/**
+	 * 
+	 * @param item
+	 */
 	void execute(final T item);
 }
